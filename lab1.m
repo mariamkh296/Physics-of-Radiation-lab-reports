386c15f09f0c298717739b23e18cd386ed741dad
mariamkh296 <mariam.720240044@ejust.edu.eg>
2025-11-27 20:52:06 +0200

    Add files via upload

---
+++ lab1.m
@@ -0,0 +1,17 @@
+% Simple CSV plotting example
+clear; close all; clc;
+
+% Read CSV file
+data = readtable("D:\Space\Space 3rd term\Radiation lab\physics of radiation lab 1.xlsx");
+
+x= data{:,1}
+y= data{:,2}
+
+% Basic line plot of all numeric columns
+figure;
+plot(x,y)
+title('Time versus Counting rate graph');
+xlabel('Time Theshold');
+ylabel('Counting rate');
+grid on;
+
